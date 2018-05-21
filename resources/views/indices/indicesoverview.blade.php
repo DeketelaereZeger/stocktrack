04ca212bcd904cf0139977b905515470ab8c6660
DeketelaereZeger <zegerdeketelaere@gmail.com>
2018-05-21 14:53:47 +0200

    blade adjustment

--- resources/views/indices/indicesoverview.blade.php
+++ resources/views/indices/indicesoverview.blade.php
@@ -13,7 +13,7 @@
 
     <div class="row">
         @foreach($indices as $indice)
-            <div class="company col-md-4 col-sm-6 mb-4">
+            <div class="company col-lg-12 col-md-6 col-sm-6 mb-4">
                 <div class=" company-card ">
                     <h2>{{$indice->name}} ({{$indice->ticker}})</h2>
                     <p>{{$indice->description}}. &#9400; Bloomberg.com</p>
