4f14c0aeff74e851ff78f126ceda5bc992a4dc38
DeketelaereZeger <zegerdeketelaere@gmail.com>
2018-05-22 18:13:02 +0200

    sw  precache minor adj

--- webpack.mix.js
+++ webpack.mix.js
@@ -26,7 +26,8 @@ mix.webpackConfig({
                 '/': ['resources/views/index/index.blade.php'],
                 '/indices': ['resources/views/indices/indice.blade.php'],
                 '/stocks': ['resources/views/companies/company.blade.php'],
-                '/indices/{ticker}': ['resources/views/indices/indice.blade.php'],
+                '/indices/AEX': ['resources/views/indices/indice.blade.php'],
+                '/indices/BEL20': ['resources/views/indices/indice.blade.php'],
                 '/stocks/{ticker}': ['resources/views/companies/companiesoverview.blade.php'],
 
             },
